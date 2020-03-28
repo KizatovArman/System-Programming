ee14480b1e36eb39f337707fc7750b19602eb569
KizatovArman <kizatov.armashka.05@gmail.com>
2020-03-28 10:44:02 +0600

    Lab 8 last

--- week9/lab/lab8/.lab8.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/arman/Desktop/SystemProgramming/System-Programming/week9/lab/lab8/lab8.mod := { echo  /home/arman/Desktop/SystemProgramming/System-Programming/week9/lab/lab8/lab8.o; echo; } > /home/arman/Desktop/SystemProgramming/System-Programming/week9/lab/lab8/lab8.mod
--- week9/lab/lab8/lab8.mod
+++
@@ -1,2 +0,0 @@
-/home/arman/Desktop/SystemProgramming/System-Programming/week9/lab/lab8/lab8.o
-
